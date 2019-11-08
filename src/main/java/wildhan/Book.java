514b03701feb6f59f18af640448289f89ae6c8a4
narashikam <narashikamarruu@gmail.com>
2019-11-08 14:31:25 +0700

    test

--- src/main/java/wildhan/Book.java
+++ src/main/java/wildhan/Book.java
@@ -3,6 +3,7 @@ package wildhan;
 
 import java.util.HashMap;
 
+// Test commit
 public class Book {
     public static int iMaxSlots = 0;
     public static HashMap<Integer, String> hmBook = new HashMap<>();
